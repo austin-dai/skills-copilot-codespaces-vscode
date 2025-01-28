38ffdec3bbc0e602f448d6cbc79686e40e93b3fb
austin-dai <159202950+austin-dai@users.noreply.github.com>
2025-01-28 04:42:09 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMembers() {
+    var members = [
+        {name: 'John', skills: ['JavaScript', 'React', 'Node']},
+        {name: 'Jane', skills: ['JavaScript', 'Angular', 'Python']},
+    ];
+    return members;
+}
